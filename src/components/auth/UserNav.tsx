18e2597e8d50982dd4b58d3643ca55e896f7c74d
Meezab Momin <meezabmomin07@gmail.com>
2025-09-11 08:25:15 +0000

    Okay since you are claming that you foucd out the problem tell me the so

--- src/components/auth/UserNav.tsx
+++ src/components/auth/UserNav.tsx
@@ -51,43 +51,41 @@ export default function UserNav() {
   }
 
   return (
-    <Dialog>
-        <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-            <Avatar className="h-8 w-8">
-                <AvatarImage src={userProfile?.photoURL || undefined} alt={userProfile?.displayName || ''} />
-                <AvatarFallback>
-                    {userProfile?.displayName?.charAt(0).toUpperCase() || <User className="h-5 w-5" />}
-                </AvatarFallback>
-            </Avatar>
-            </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-            <DropdownMenuLabel className="font-normal">
-            <div className="flex flex-col space-y-1">
-                <p className="text-sm font-medium leading-none">{userProfile?.displayName}</p>
-                <p className="text-xs leading-none text-muted-foreground">
-                {userProfile?.email}
-                </p>
-            </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-                <DropdownMenuItem asChild>
-                    <Link href="/profile">
-                        <User className="mr-2 h-4 w-4" />
-                        <span>Profile</span>
-                    </Link>
-                </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={signOut}>
-            <LogOut className="mr-2 h-4 w-4" />
-            <span>Log out</span>
-            </DropdownMenuItem>
-        </DropdownMenuContent>
-        </DropdownMenu>
-    </Dialog>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src={userProfile?.photoURL || undefined} alt={userProfile?.displayName || ''} />
+            <AvatarFallback>
+              {userProfile?.displayName?.charAt(0).toUpperCase() || <User className="h-5 w-5" />}
+            </AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{userProfile?.displayName}</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {userProfile?.email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <Link href="/profile">
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={signOut}>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
